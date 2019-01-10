69d424666d3f0e3fb39fec18878925194e1e3075
Olivers De Abreu <olivers.deabreu@engiefactory.com>
2019-01-10 16:51:43 -0300

    Fix styles

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,6 +2,45 @@ import React from 'react';
 
 import './style.css';
 
+const tareas = [
+  {
+    id: 1,
+    title: 'Hacer curso de React',
+    body: 'Crear la presentación y los ejercicios de ejemplo',
+    tags: ['react', 'curso'],
+    done: false,
+    createdAt: '2019-01-10T14:41:52.566Z',
+    finishedAt: null
+  },
+  {
+    id: 2,
+    title: 'Hacer curso de React',
+    body: 'Crear la presentación y los ejercicios de ejemplo',
+    tags: ['react', 'curso'],
+    done: false,
+    createdAt: '2019-01-10T14:41:52.566Z',
+    finishedAt: null
+  },
+  {
+    id: 3,
+    title: 'Hacer curso de React',
+    body: 'Crear la presentación y los ejercicios de ejemplo',
+    tags: ['react', 'curso'],
+    done: false,
+    createdAt: '2019-01-10T14:41:52.566Z',
+    finishedAt: null
+  },
+  {
+    id: 4,
+    title: 'Hacer curso de React',
+    body: 'Crear la presentación y los ejercicios de ejemplo',
+    tags: ['react', 'curso'],
+    done: false,
+    createdAt: '2019-01-10T14:41:52.566Z',
+    finishedAt: null
+  }
+]
+
 const Home = () => (
   <div className="main">
     <h1>Mi Lista</h1>
